21dbcb517caa70d79f267f280161cbce34e02ae4
Psalasmiranda <pablo.salasm@utem.cl>
2017-06-13 19:20:19 -0400

    insumo

--- clinica/config/routes.rb
+++ clinica/config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
 
-  get  'insumos/', to: 'insumos#index'
-  get  'insumos/:id', to: 'insumos#mostrar' , as: 'insumo'
-  get  'insumos/nuevo', to: 'insumos#nuevo' , as: 'nuevo_insumo'
-  get  'insumos/:id/editar', to: 'insumos#editar' , as: 'editar_insumo'
-  post 'insumos/', to: 'insumos#crear'
+
+  get 'insumos/', to: 'insumos#index'
+  get 'insumos/nuevo', to: 'insumos#nuevo', as: 'nuevo_insumo'
+  get 'insumos/:id',to:'insumos#mostrar', as: 'insumo'
+  get 'insumos/:id/editar',to: 'insumos#editar', as: 'editar_insumo'
   put 'insumos/:id' ,to: 'insumos#update'
   patch 'insumos/:id' ,to: 'insumos#update'
-  delete 'insumos/:id',to: 'insumos#eliminar'
+  post 'insumos/', to: 'insumos#crear'
+delete 'insumos/:id',to: 'insumos#eliminar'
 end
