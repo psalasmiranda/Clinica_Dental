d6a89904e6dfec5dbdd117b93831ef6e2f812fd1
Psalasmiranda <pablo.salasm@utem.cl>
2017-06-13 18:54:40 -0400

    insumo

--- clinica/app/controllers/insumos_controller.rb
+++ clinica/app/controllers/insumos_controller.rb
@@ -1,7 +1,46 @@
 class InsumosController < ApplicationController
-
+before_action :set_insumo, only: [:mostrar, :editar, :update, :eliminar]
   def index
-    @insumoss = Insumo.all
+    @insumos= Insumo.all
   end
 
+  def mostrar
+  end
+
+  def nuevo
+    @insumo= Insumo.new
+  end
+
+  def editar
+  end
+
+  def crear
+      @insumo = Insumo.create(insumo_params)
+      respond_to do |format|
+        if @insumo.save
+          format.html {redirect_to @insumo, notice: 'Fue creado con mucho exito'}
+        else
+          format.html {render :nuevo}
+        end
+      end
+  end
+  def update
+      respond_to do |format|
+        if @insumo.update(insumo_params)
+          format.html {redirect_to @insumo}
+        else
+          format.html {render :nuevo}
+        end
+      end
+  end
+
+  private
+
+  def  set_insumo
+    @insumo= Insumo.find(params[:id])
+  end
+
+  def insumo_params
+    params.require(:insumo).permit(:nombre, :cantidad, :costo, :ingreso, :vencimiento)
+  end
 end
--- clinica/config/routes.rb
+++ clinica/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'bodega/', to: 'insumos#index'
 
+  get  'insumos/', to: 'insumos#index'
+  get  'insumos/:id', to: 'insumos#mostrar' , as: 'insumo'
+  get  'insumos/nuevo', to: 'insumos#nuevo' , as: 'nuevo_insumo'
+  get  'insumos/:id/editar', to: 'insumos#editar' , as: 'editar_insumo'
+  post 'insumos/', to: 'insumos#crear'
+  put 'insumos/:id' ,to: 'insumos#update'
+  patch 'insumos/:id' ,to: 'insumos#update'
+  delete 'insumos/:id',to: 'insumos#eliminar'
 end
