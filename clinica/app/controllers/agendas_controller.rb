8a01eb55e8fc6f188f58adeb14eb79bc77a5d5bf
johannalizm <johanna.munozi@utem.cl>
2017-06-13 21:10:22 -0400

    Vista Agenda

---
+++ clinica/app/controllers/agendas_controller.rb
@@ -0,0 +1,50 @@
+class AgendasController < ApplicationController
+  before_action :set_agenda, only: [:mostrar, :editar, :update, :eliminar]
+    def index
+      @agendas= Agenda.all
+    end
+
+    def mostrar
+    end
+
+    def nuevo
+      @agenda= Agenda.new
+    end
+
+    def editar
+    end
+    def crear
+        @agenda = Agenda.create(agenda_params)
+        respond_to do |format|
+          if @agenda.save
+            format.html {redirect_to @agenda, notice: 'Fue creado con mucho exito'}
+          else
+            format.html {render :nuevo}
+          end
+        end
+    end
+    def update
+        respond_to do |format|
+          if @agenda.update(agenda_params)
+            format.html {redirect_to @agenda}
+          else
+            format.html {render :nuevo}
+          end
+        end
+    end
+    def eliminar
+        @agenda.destroy
+        respond_to do |format|
+          format.html {redirect_to agendas_url, notice: 'fue eliminado'}
+        end
+    end
+    private
+
+    def  set_agenda
+      @agenda= Agenda.find(params[:id])
+    end
+
+    def agenda_params
+      params.require(:agenda).permit(:hora, :costo)
+    end
+end
--- clinica/config/routes.rb
+++ clinica/config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
 
+  get 'agendas/', to: 'agendas#index'
+  get 'agendas/nuevo', to: 'agendas#nuevo', as: 'nuevo_agenda'
+  get 'agendas/:id',to:'agendas#mostrar', as: 'agenda'
+  get 'agendas/:id/editar',to: 'agendas#editar', as: 'editar_agenda'
+  put 'agendas/:id' ,to: 'agendas#update'
+  patch 'agendas/:id' ,to: 'agendas#update'
+  post 'agendas/', to: 'agendas#crear'
+  delete 'agendas/:id',to: 'agendas#eliminar'
 
-  get  'insumos/', to: 'insumos#index'
-  get  'insumos/:id', to: 'insumos#mostrar' , as: 'insumo'
-  get  'insumos/nuevo', to: 'insumos#nuevo' , as: 'nuevo_insumo'
-  get  'insumos/:id/editar', to: 'insumos#editar' , as: 'editar_insumo'
-  post 'insumos/', to: 'insumos#crear'
+  get 'insumos/', to: 'insumos#index'
+  get 'insumos/nuevo', to: 'insumos#nuevo', as: 'nuevo_insumo'
+  get 'insumos/:id',to:'insumos#mostrar', as: 'insumo'
+  get 'insumos/:id/editar',to: 'insumos#editar', as: 'editar_insumo'
   put 'insumos/:id' ,to: 'insumos#update'
   patch 'insumos/:id' ,to: 'insumos#update'
+  post 'insumos/', to: 'insumos#crear'
   delete 'insumos/:id',to: 'insumos#eliminar'
+
 end
