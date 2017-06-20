f508e222fc7531fc27c38e22d0b1ced6d793bb35
icamila <icamilah.qrefr@gmail.com>
2017-06-20 00:05:01 -0400

    Asociones Tratamientos, Historia, Pacientes, Comuna, Agenda, Area

--- clinica/app/models/agenda.rb
+++ clinica/app/models/agenda.rb
@@ -1,4 +1,6 @@
 class Agenda < ApplicationRecord
+  has_many :pacientes
+  belongs_to :area, foreign_key: :area_id
   validates :hora, presence: {message => "LLenado Obligatorio"}
   validates :costo, presence: {message => "LLenado Obligatorio"}
 end
--- clinica/app/models/tratamiento.rb
+++ clinica/app/models/tratamiento.rb
@@ -1,4 +1,5 @@
 class Tratamiento < ApplicationRecord
+  has_many :historias
   validates :nombre, presence: {:message => "Llenado Obligatorio"}
   validates :descripcion, presence: {:message => "Llenado Obligatorio"}
 end
