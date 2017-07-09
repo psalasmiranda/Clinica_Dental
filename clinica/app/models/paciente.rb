1ee6f9162fedbef9a61d7efa06309c2cd98c7082
johannalizm <johanna.munozi@utem.cl>
2017-07-09 16:58:50 -0400

    Validacion Ruty Direccion, otros

--- clinica/app/models/paciente.rb
+++ clinica/app/models/paciente.rb
@@ -2,10 +2,14 @@ class Paciente < ApplicationRecord
   has_many :historias
   belongs_to :comuna, foreign_key: :comuna_id
   has_many :agendas
+
   validates :rut, presence: {:message => "Llenado Obligatorio"}
+  validates :rut,uniqueness: true, rut: true
 
   validates :nombre, presence: {:message => "LLenado Obligatorio"}
   validates :nombre, format:{with: /\A[a-zA-Z]+\z/,message: "Solo acepta letras"}
+  validates :nombre, length: {minimum: 3, maximum: 15, :message => "El nombre debe tener entre 3 y 15 caracteres"}
+
 
   validates :materna, presence: {:message => "LLenado Obligatorio"}
   validates :materna, format:{with: /\A[a-zA-Z]+\z/,message: "Solo acepta letras"}
@@ -16,9 +20,13 @@ class Paciente < ApplicationRecord
   validates :sexo, presence: {:message => "LLenado Obligatorio"}
 
   validates :telefono, presence: {:message => "LLenado Obligatorio"}
+  validates :telefono, numericality: { :greater_than_or_equal_to => 0, message: "Numeros que parten de 0 no validos"}
+  validates :telefono, numericality: {only_integer: true, message: "Solo numeros"}
+  validates :telefono, length: {is: 11, message: "EL telefono debe tener 11 digitos"}
 
   validates :direccion, presence: {:message => "LLenado Obligatorio"}
   
+
   validates :edad, presence: {:message => "LLenado Obligatorio"}
   validates :edad, numericality: {only_integer: true, message:"Solo numero enteros"}
   validates :edad, numericality: {:greater_than_or_equal_to =>1, message: "No se aceptan menores a 1 año"}
@@ -28,7 +36,7 @@ class Paciente < ApplicationRecord
 
   def edad_validacion
     if self.edad.present?
-      if self.edad >= 100
+      if self.edad > 100
         errors.add(:edad, "no puede ser mayor a 100 años")
         return false
       end
