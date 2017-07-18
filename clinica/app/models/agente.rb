426d5515d56f8a4f8828e72dd768c4f686418909
johannalizm <johanna.munozi@utem.cl>
2017-07-18 19:06:19 -0400

    Validaciones

--- clinica/app/models/agente.rb
+++ clinica/app/models/agente.rb
@@ -2,6 +2,8 @@ class Agente < ApplicationRecord
   has_many :agentes
   validates :nombre, presence: {:message => "Llenado Obligatorio"}
   validates :nombre, format:{with: /\A[a-zA-Z \s]+\z/,message: "Solo acepta letras"}
+  validates :nombre, length: {minimum: 3, maximum: 15, :message => "El nombre debe tener entre 3 y 15 caracteres"}
+
 
   validates :apellidos, presence: {:message => "Llenado Obligatorio"}
   validates :apellidos, format:{with: /\A[a-zA-Z]+\z/,message: "Solo acepta letras"}
@@ -18,7 +20,8 @@ class Agente < ApplicationRecord
 
 
   validates :dirrecion, presence: {:message => "Llenado Obligatorio"}
-  validates :dirrecion, format: {with: /\A[0-9a-zA-Z\s}]+\z/,message: "Direccion no valida"}
+  validates :dirrecion, format: {with: /\A[0-9a-zA-Z\s}]+\z/,message: "Direccion no valida no acepta #"}
+  validates :dirrecion, length: {minimum: 7, maximum: 30, :message => "La direccion debe tener entre 7 y 30 caracteres"}
 
 
 
