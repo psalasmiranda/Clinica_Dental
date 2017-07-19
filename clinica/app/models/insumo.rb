69204d95dae03e6f46f482a48daf7593d3aeeb2e
icamila <camila.caceresr@utem.cl>
2017-07-19 03:39:19 -0400

    Validacion Insumos2

--- clinica/app/models/insumo.rb
+++ clinica/app/models/insumo.rb
@@ -13,8 +13,15 @@ class Insumo < ApplicationRecord
   validates :costo, presence: {:message => "Llenado Obligatorio"}
   validates :costo, numericality: {only_integer: true, message: "Solo numero enteros"},length: {minimun:1, maximum:6, message: "El precio debe ser de 1 a 6 digitos"}
   validates :costo, numericality: {:greater_than_or_equal_to =>0, message: "No se aceptan enteros negativos"}
+  validate :Validate_Vencimiento
 
+  def Validate_Vencimiento
+    if self.ingreso > self.vencimiento
+      errors.add(:vencimiento,"La fecha de vencimiento no puede ser menor que la fecha de ingreso")
+      return false
+    end
 
+  end
 
 
 end
