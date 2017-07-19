4b2975d990fa8b1bcbe1233d51b33ac0a27504fe
icamila <camila.caceresr@utem.cl>
2017-07-18 23:38:07 -0400

    actualizacion sesion

--- clinica/config/initializers/session_store.rb
+++ clinica/config/initializers/session_store.rb
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-#Rails.application.config.session_store :cookie_store, key: '_my_session', :expire_after => 3.minutes
-=======
+
 Rails.application.config.session_store :cookie_store, key: '_my_session', :expire_after => 60.minutes
->>>>>>> 0561758c8f8d5acbd585282ed14927c815f5b28b
