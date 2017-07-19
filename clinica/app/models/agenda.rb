a4ae080f0d72ff1a3e9a6bda86a530940172b5cd
icamila <camila.caceresr@utem.cl>
2017-07-19 00:40:15 -0400

    se soluciona error de agenda

--- clinica/app/models/agenda.rb
+++ clinica/app/models/agenda.rb
@@ -5,15 +5,4 @@ class Agenda < ApplicationRecord
   belongs_to :usuario, foreign_key: :usuario_id
   validates :hora, presence: {:message => "LLenado Obligatorio"}
 
-
-  validates :costo, presence: {:message => "LLenado Obligatorio"}
-  validates :costo, numericality: {only_integer: true, message:"Solo numero enteros"}
-  validates :costo, numericality: {:greater_than_or_equal_to =>0, message: "No se aceptan enteros negativos"},length: {minimun:1, maximum:6, message: "El precio debe ser de 1 a 6 digitos"}
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 09833a33fa88ff6bbb2595dd001f53accc08ce83
-  validates :hora, presence: {:message => "LLenado Obligatorio"}
-
 end
