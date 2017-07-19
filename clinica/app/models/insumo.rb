16de882823daa9b9d520ad8c55b640012514da50
icamila <camila.caceresr@utem.cl>
2017-07-19 16:05:08 -0400

    Actualizacion

--- clinica/app/models/insumo.rb
+++ clinica/app/models/insumo.rb
@@ -9,7 +9,6 @@ class Insumo < ApplicationRecord
   validates :cantidad, numericality: {:greater_than_or_equal_to =>0, message: "No se aceptan enteros negativos"}
   validates :cantidad, length: { in: 1..2, message: "La dimension de cantidad no puede superar las 99 unidades"}
 
-
   validates :costo, presence: {:message => "Llenado Obligatorio"}
   validates :costo, numericality: {only_integer: true, message: "Solo numero enteros"},length: {minimun:1, maximum:6, message: "El precio debe ser de 1 a 6 digitos"}
   validates :costo, numericality: {:greater_than_or_equal_to =>0, message: "No se aceptan enteros negativos"}
