304f0049e52f59ff258c6ba61f565718534ed06e
icamila <camila.caceresr@utem.cl>
2017-07-30 03:14:17 -0400

    especialidades

--- clinica/app/assets/javascripts/especialidadJQuery.js
+++ clinica/app/assets/javascripts/especialidadJQuery.js
@@ -10,4 +10,53 @@ $(document).on("ready", function(){
   $('.move-right').trigger("click");
 
 
-})
+});
+$(function () {
+
+    $('body').on('click', '.list-group .list-group-item', function () {
+        $(this).toggleClass('active');
+    });
+    $('.list-arrows button').click(function (e) {
+        e.preventDefault();
+        var $button = $(this), actives = '';
+        if ($button.hasClass('move-left')) {
+            actives = $('.list-right ul li.active');
+            actives.clone().appendTo('.list-left ul');
+            actives.remove();
+            $.each(actives, function( index, value ) {
+              var elemento = $(value).text();
+              $("#usuario_"+elemento).attr("value", "false");
+            });
+        } else if ($button.hasClass('move-right')) {
+            actives = $('.list-left ul li.active');
+            actives.clone().appendTo('.list-right ul');
+            actives.remove();
+            $.each(actives, function( index, value ) {
+              var elemento = $(value).text();
+              $("#usuario_"+elemento).attr("value", "true");
+            });
+        }
+    });
+    $('.dual-list .selector').click(function () {
+        var $checkBox = $(this);
+        if (!$checkBox.hasClass('selected')) {
+            $checkBox.addClass('selected').closest('.well').find('ul li:not(.active)').addClass('active');
+            $checkBox.children('i').removeClass('glyphicon-unchecked').addClass('glyphicon-check');
+        } else {
+            $checkBox.removeClass('selected').closest('.well').find('ul li.active').removeClass('active');
+            $checkBox.children('i').removeClass('glyphicon-check').addClass('glyphicon-unchecked');
+        }
+    });
+    $('[name="SearchDualList"]').keyup(function (e) {
+        var code = e.keyCode || e.which;
+        if (code == '9') return;
+        if (code == '27') $(this).val(null);
+        var $rows = $(this).closest('.dual-list').find('.list-group li');
+        var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+        $rows.show().filter(function () {
+            var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+            return !~text.indexOf(val);
+        }).hide();
+    });
+
+});
