9c3c1a4415d69d57ee72a2442477360cb497b750
icamila <camila.caceresr@utem.cl>
2017-07-30 20:13:36 -0400

    Tratamientos 2.0

--- clinica/app/models/tratamiento.rb
+++ clinica/app/models/tratamiento.rb
@@ -10,6 +10,10 @@ class Tratamiento < ApplicationRecord
   validates :descripcion, format:{with: /\A[0-9a-zA-Z\s]+\z/,message: "Solo acepta letras"}
   validates :descripcion, length: {minimum: 7, maximum: 30, :message => "La descripcion debe tener entre 7 y 30 caracteres"}
 
+  validates :prevision, presence: {:message => "Llenado Obligatorio"}
+  validates :prevision, format:{with: /\A[0-9a-zA-Z\s]+\z/,message: "Solo acepta letras"}
+  validates :prevision, length: {minimum: 3, maximum: 15, :message => "El nombre debe tener entre 3 y 15 caracteres"}
+
   validates :costo, presence: {:message => "Llenado Obligatorio"}
   validates :costo, numericality: {only_integer: true, message: "Solo valores enteros"},length: {minimun:1, maximum:8, message: "El precio debe ser de 1 a 8 digitos"}
   validates :costo, numericality: {:greater_than_or_equal_to =>0, message: "No se aceptan valores negativos"}
