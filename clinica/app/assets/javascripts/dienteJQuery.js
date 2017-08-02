e81f4815c55ce702473afd7491c6901565035a61
icamila <camila.caceresr@utem.cl>
2017-08-01 20:19:21 -0400

    Se soluciona conflicto con las imagenes

--- clinica/app/assets/javascripts/dienteJQuery.js
+++ clinica/app/assets/javascripts/dienteJQuery.js
@@ -1,18 +1,39 @@
+
 $(document).on("ready", function(){
 
   var piezas = $("div #contenidoOculto").data("dientes");
-  $(".diente img").each(function(index, elemento){
+  $(".diente img.Spieza").each(function(index, elemento){
     var numeroID = $(elemento).attr("id");
     if(eval("piezas.d"+numeroID) == null || eval("piezas.d"+numeroID) == false){
-      $(elemento).attr("src", "/assets/diente"+numeroID+".jpg");
-      $.data(elemento,"seleccionado","si");
+      $(elemento).hide();
     }else if(eval("piezas.d"+numeroID) == true){
-      $(elemento).attr("src", "/assets/Sdiente"+numeroID+".jpg");
-      $.data(elemento,"seleccionado","no")
+      $(elemento).show();
+    }
+  });
+  $(".diente img.pieza").each(function(index, elemento){
+    var numeroID = $(elemento).attr("id");
+    if(eval("piezas.d"+numeroID) == null || eval("piezas.d"+numeroID) == false){
+        $(elemento).show();
+        //$.data(elemento,"seleccionado","si");
+    }else if(eval("piezas.d"+numeroID) == true){
+        $(elemento).hide();
+        //$.data(elemento,"seleccionado","no");
     }
   });
 
   $('.pieza').on("click", function(){
+    var numeroID = $(this).attr("id");
+    $(this).next().show();
+    $(this).hide();
+    $("#agenda_d"+numeroID).attr("value", "true");
+  });
+  $('.Spieza').on("click", function(){
+    var numeroID = $(this).attr("id");
+    $(this).prev().show();
+    $(this).hide();
+    $("#agenda_d"+numeroID).attr("value", "false");
+  });
+  /*$('.pieza').on("click", function(){
     var id = $(this).attr("id");
     if($.data(this,"seleccionado")== "no"){
       $(this).attr("src", "/assets/diente"+id+".jpg");
@@ -23,5 +44,5 @@ $(document).on("ready", function(){
       $.data(this,"seleccionado","no");
       $("#agenda_d"+id).attr("value", "true");
     }
-  })
+  })*/
 })
